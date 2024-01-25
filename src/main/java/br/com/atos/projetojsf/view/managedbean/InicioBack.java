5baf04972e8e01e4cd50008b90718789a3a520dd
Rogerio Napoleao Jr <rogerionj@gmail.com>
2024-01-25 14:23:45 -0300

    first commit

---
+++ src/main/java/br/com/atos/projetojsf/view/managedbean/InicioBack.java
@@ -0,0 +1,26 @@
+package br.com.atos.projetojsf.view.managedbean;
+
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "inicio")
+public class InicioBack {
+
+	private String mensagem;
+	
+	
+	public InicioBack() {
+		this.mensagem = "Olá Managed Bean";
+	}
+	
+	
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+}
+	
